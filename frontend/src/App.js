2ac3e36d3d96d54450b6d65acbac13bf95a7bb4b
MD MOQADDAS <prodshell.com@gmail.com>
2024-02-28 16:02:49 +0530

    fixed the .github.workflows/frontend.yml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  
+  const [name, setName] = useState('');
+  const [response, setResponse] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
